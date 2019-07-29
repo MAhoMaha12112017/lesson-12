19cf107e97a796950840f0f3e2dd7fa154f37600
Matti Aho <ookooko@gmail.com>
2019-07-29 09:29:14 +0300

    reselect / selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-import {connect} from 'react-redux'
+import {selectCartItems} from '../../redux/cart/cart.selectors';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect'
 
 const CartDropdown = ({cartItems}) => (
   <div className='cart-dropdown'>
@@ -15,8 +17,8 @@ const CartDropdown = ({cartItems}) => (
   </div>
 )
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  cartItems
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
